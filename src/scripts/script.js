05a6050b91dffb53d1d5d8f91df7213386716c02
Thiago Arques <thiagoarques2@gmail.com>
2023-06-02 22:25:40 -0300

    Finalizado

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,19 @@
-/* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function modalContainer(){
+    const button = document.querySelector(".buttonHeader");
+    const modalContainer = document.querySelector(".modalContainer");
+
+    button.addEventListener('click', () => {
+        modalContainer.showModal();
+        closeModal();
+    });
+}
+modalContainer();
+
+function closeModal(){
+    const button = document.querySelector('.buttonCloseModal');
+    const modalContainer = document.querySelector('.modalContainer');
+
+    button.addEventListener('click', () => {
+        modalContainer.close();
+    });
+}
